36808eb2a78c1e06cfd02a5cb1947dcc25c7d78d
Ronny Medina <ronnyzte@gmail.com>
2026-04-27 20:06:46 -0300

    feat(modules): wire CacheModule globally and TimezoneService into RestaurantsModule

--- apps/api-core/src/restaurants/restaurants.module.ts
+++ apps/api-core/src/restaurants/restaurants.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { RestaurantsService } from './restaurants.service';
 import { RestaurantRepository } from './restaurant.repository';
 import { RestaurantsController } from './restaurants.controller';
+import { TimezoneService } from './timezone.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [RestaurantsController],
-  providers: [RestaurantsService, RestaurantRepository],
-  exports: [RestaurantsService, RestaurantRepository],
+  providers: [RestaurantsService, RestaurantRepository, TimezoneService],
+  exports: [RestaurantsService, RestaurantRepository, TimezoneService],
 })
 export class RestaurantsModule {}
